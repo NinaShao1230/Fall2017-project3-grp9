ebd120382e35e60ac34e25fd7f2de933248d53a9
aprilxwang <xw2439@columbia.edu>
2017-11-01 09:59:47 -0400

    rf new
    remove the impute function

--- lib/rf_features.R
+++ lib/rf_features.R
@@ -7,20 +7,25 @@ lbp_features <- read.csv('~/Desktop/ADS/Fall2017-project3-fall2017-project3-grp9
 features3 <- cbind(sift_features, color_features, lbp_features)
 labels3 = unlist(read.csv('~/Desktop/ADS/Fall2017-project3-fall2017-project3-grp9/data/training_set/label_train.csv', header = T)[,2])
 
-# May be deleted later:
-begin <- Sys.time()
-imputed3 <- rfImpute(features3, as.factor(labels3), ntree = 100, iter = 1)
-end <- Sys.time()
-end - begin
+# # May be deleted later:
+# begin <- Sys.time()
+# imputed3 <- rfImpute(features3, as.factor(labels3), ntree = 100, iter = 1)
+# end <- Sys.time()
+# end - begin
+
+# Data Cleaning/Preprocessing: Replace NAs with ColMean
+na_ind <- which(is.na(features3),TRUE)
+range(na_ind[,2])
+features3[] <- lapply(features3, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x))
 
 set.seed(90)
-n <- nrow(imputed3)
+n <- nrow(features3)
 index <- sample(n, n*0.75)
-train_features <- imputed3[index,-1]
-train_labels <- as.factor(imputed3[index,1])
+train_features <- features3[index,-1]
+train_labels <- as.factor(features3[index,1])
 
-test_features <- imputed3[-index,-1]
-test_labels <- as.factor(imputed3[-index,1])
+test_features <- features3[-index,-1]
+test_labels <- as.factor(features3[-index,1])
 
 ### CV ###
 K = 5
@@ -43,7 +48,7 @@ for (i in 1:K){
 # Visualize CV
 # ggplot(data = data.frame(cv.error)) + geom_point(aes(x = 1:10, y = cv.error))
 
-best <- which.min(cv.error)
+error <- mean(cv.error)
 
 system.time(rf.train3 <- tuneRF(train_features[s != best,], as.factor(train_labels[s != best]), ntreeTry = 100, doBest = T))
 #1116.943 sec
